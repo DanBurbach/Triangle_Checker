9145222e5142a69bea7eff4e7277944950d68ae4
Christopher Cahill and Dan Burbach <dan@>
2018-11-26 13:15:55 -0800

    added radio buttons for distance cost

--- parcels/app.rb
+++ parcels/app.rb
@@ -1,9 +1,12 @@
+require ('rubygems')
 require ('sinatra')
 require ('sinatra/reloader')
 also_reload ('lib/**/*.rb')
 require('./lib/parcels')
+# require ('loc')
 
 get ('/') do
+  # require ('loc')
   @description = "this application will determine the price of your package"
   erb(:input)
 end
@@ -13,8 +16,15 @@ get ('/output') do
   @width = params.fetch("width")
   @height = params.fetch("height")
   @weight = params.fetch("weight")
-  parcel = Parcel.new(@length, @width, @height, @weight)
+  @distance = params.fetch("distance").to_i
+  # @shipfrom = params.fetch("shipfrom")
+  # @shipto = params.fetch("shipto")
+  parcel = Parcel.new(@length, @width, @height, @weight, @distance)
   @volume = parcel.volume
   @weight = parcel.weight
+  @price = parcel.price
+  @distance = parcel.distance
+  # @distance = parcel.shipdistance
+  # @distance = @shipfrom.distance_to(@shipto)
   erb(:output)
 end
--- parcels/lib/parcels.rb
+++ parcels/lib/parcels.rb
@@ -1,22 +1,35 @@
+
 class Parcel
-  attr_accessor(:length, :width, :height, :weight, :volume)
-  def initialize(length, width, height, weight)
+  # require('loc')
+  attr_accessor(:length, :width, :height, :weight, :volume, :price, :distance)
+  def initialize(length, width, height, weight, distance)
     @length = length.to_i
     @width = width.to_i
     @height = height.to_i
     @weight = weight.to_i
+    @distance = distance.to_i
 
   end
 
   def volume
     if (self.length == 0) || (self.width == 0) || (self.height == 0)
-      "you need to enter a whole number for parcel dimensions"
+      "not a whole number for parcel dimensions."
     else
       self.volume = (self.length * self.width * self.height)
     end
   end
 
-  def price?
-
+  def price
+    if self.distance == 1
+      self.volume * self.weight * 0.25
+    elsif self.distance == 2
+      self.volume * self.weight * 0.5
+    elsif self.distance == 5
+      self.volume * self.weight * 1.25
+    elsif self.distance == 20
+      self.volume * self.weight * 5.75
+    elsif self.distance == 100
+      self.volume * self.weight * 20.45
+    end
   end
 end
